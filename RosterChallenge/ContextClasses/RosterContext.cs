741e19931d78e4e6fe30ad5446620cf653c17bc1
Curtis <gouldcurtis0@gmail.com>
2021-07-24 15:52:32 -0400

    clean up home page, create model representing data provided as well as additional field to mark payments as paid, create initial database using EF code first

---
+++ RosterChallenge/ContextClasses/RosterContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RosterChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RosterChallenge.ContextClasses
+{
+    public class RosterContext : DbContext
+    {
+        public RosterContext(): base()
+        {
+
+        }
+        public RosterContext(DbContextOptions<RosterContext> options) : base(options)
+        { 
+        
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
---
+++ RosterChallenge/Models/RosterModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RosterChallenge.Models
+{
+    public class RosterModel
+    {
+        public RosterModel()
+        {
+
+        }
+        public List<Artist> Roster;
+    }
+
+    public class Artist
+    {
+        [Required(ErrorMessage = "ID is required."), Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Artist Name is required.")]
+        public string artist { get; set; }
+
+        [Required(ErrorMessage = "Rate is required.")]
+        public double rate { get; set; }
+
+        [Required(ErrorMessage = "Total Streams is required.")]
+        public int streams { get; set; }
+
+        [Required(ErrorMessage = "Paid is required.")]
+        public bool paid { get; set; }
+
+        private DateTime startDate = new DateTime(2006, 4, 1);
+        private DateTime endDate = DateTime.Now;
+        private int months => ((startDate.Year - endDate.Year) * 12) + startDate.Month - endDate.Month;
+
+        public double getAverageMonthlyPayout() => getTotalPayout() / months;
+        public double getTotalPayout() => rate * streams;
+    }
+}
