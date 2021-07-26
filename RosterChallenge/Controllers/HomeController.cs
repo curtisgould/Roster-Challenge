9a5764b921d052ef31e72edacdc4fe6f20fd45ff
Curtis <gouldcurtis0@gmail.com>
2021-07-25 23:42:26 -0400

    fix boostrap migration issues, add CRUD functions, add tablesorter, add confirm message box (used for delete confirm)

--- RosterChallenge/Controllers/HomeController.cs
+++ RosterChallenge/Controllers/HomeController.cs
@@ -28,6 +28,88 @@ namespace RosterChallenge.Controllers
             return View(roster);
         }
 
+        [HttpPost]
+        public JsonResult AddArtist(string artist, double rate, int streams, bool paid)
+        {
+            try
+            {
+                Artist _artist = new Artist
+                {
+                    artist = artist,
+                    streams = streams,
+                    rate = rate,
+                    paid = paid
+                };
+
+                _rosterContext.Add(_artist);
+                _rosterContext.SaveChanges();
+
+                return Json(new { success = true, ID = _artist.ID});
+            }
+            catch (Exception e)
+            {
+                return Json(new { success = false, error = e.Message });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult UpdateArtist(int ID, string artist, double rate, int streams, bool paid)
+        {
+            try
+            {
+                Artist _artist = new Artist
+                {
+                    ID = ID,
+                    artist = artist,
+                    streams = streams,
+                    rate = rate,
+                    paid = paid
+                };
+
+                _rosterContext.Update(_artist);
+                _rosterContext.SaveChanges();
+
+                return Json(new { success = true, ID = _artist.ID });
+            }
+            catch (Exception e)
+            {
+                return Json(new { success = false, error = e.Message });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult getArtistByID(int ID)
+        {
+            Artist artist = _rosterContext.Artists.Where(m => m.ID == ID).FirstOrDefault();
+            return Json(new { success = true, artist = artist });
+        }
+
+        [HttpPost]
+        public JsonResult updatePaidArtistByID(int ID, bool paid)
+        {
+            Artist artist = _rosterContext.Artists.Where(m => m.ID == ID).FirstOrDefault();
+            artist.paid = paid;
+            _rosterContext.Update(artist);
+            _rosterContext.SaveChanges();
+            return Json(new { success = true });
+        }        
+
+        [HttpPost]
+        public JsonResult deleteArtistByID(int ID)
+        {
+            Artist artist = _rosterContext.Artists.Where(m => m.ID == ID).FirstOrDefault();
+            _rosterContext.Remove(artist);
+            _rosterContext.SaveChanges();
+            return Json(new { success = true });
+        }
+
+        [HttpPost]
+        public PartialViewResult getArtistRow(int ID)
+        {
+                Artist artist = _rosterContext.Artists.Where(m => m.ID == ID).FirstOrDefault();
+                return PartialView("~/Views/Home/Partials/_rosterRow.cshtml", artist);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
