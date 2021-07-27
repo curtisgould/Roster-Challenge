dc28c1b0ca4dd67c1f76b23d866cd39c0a518e37
Curtis <gouldcurtis0@gmail.com>
2021-07-26 20:17:13 -0400

    move reusable modal's to their own script files, hide modal backgrounds by default

---
+++ RosterChallenge/wwwroot/js/confirmModal.js
@@ -0,0 +1,25 @@
+﻿function closeConfirmModal() {
+    $("#confirmModal.modal").hide();
+    $("#confirmBackdrop.modal-backdrop").hide();
+}
+
+function openConfirmModal(title, message, yesFunc, noFunc) {
+    $("#confirmModal .modal-header").text(title);
+    $("#confirmModal .modal-body").text(message);
+    $("#confirmModal.modal").show();
+    $("#confirmBackdrop.modal-backdrop").show();
+
+    if (typeof yesFunc !== 'function') {
+        yesFunc = function () { };
+    }
+
+    if (typeof noFunc !== 'function') {
+        noFunc = function () { };
+    }
+
+    $("#ConfirmYesButton").off();
+    $("#ConfirmYesButton").on("click", function () { yesFunc(); closeConfirmModal(); });
+
+    $("#ConfirmNoButton").off();
+    $("#ConfirmNoButton").on("click", function () { noFunc(); closeConfirmModal(); });
+}
---
+++ RosterChallenge/wwwroot/js/notificationModal.js
@@ -0,0 +1,18 @@
+﻿function closeNotificationModal() {
+    $("#NotificationModal.modal").hide();
+    $("#NotificationBackdrop.modal-backdrop").hide();
+}
+
+function openNotificationModal(title, message, OKFunc) {
+    $("#NotificationModal .modal-header").text(title);
+    $("#NotificationModal .modal-body").text(message);
+    $("#NotificationModal.modal").show();
+    $("#NotificationBackdrop.modal-backdrop").show();
+
+    if (typeof OKFunc !== 'function') {
+        OKFunc = function () { };
+    }
+
+    $("#NotificationOKButton").off();
+    $("#NotificationOKButton").on("click", function () { OKFunc(); closeNotificationModal(); });
+}
